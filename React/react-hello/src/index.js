63374e688759be479d774e6f5af0e58e5b4fd1d3
NurcihaneKoroglu <nurcihanekoroglu@marun.edu.tr>
2019-06-11 17:50:34 +0300

    LETTERS CHANGED

--- React/react-hello/src/index.js
+++ React/react-hello/src/index.js
@@ -36,62 +36,10 @@ class Board extends React.Component {
            {this.renderSquare(7)}
           {this.renderSquare(8)}
           {this.renderSquare(9)}
-          {this.renderSquare(10)}
-           {this.renderSquare(11)}
            {this.renderSquare(12)}
         </div>
 
-         <div className="board-row">
 
-
-
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
-          {this.renderSquare(15)}
-          {this.renderSquare(16)}
-               {this.renderSquare(17)}
-        </div>
-
-        <div className="board-row">
-
-
-
-          {this.renderSquare(18)}
-          {this.renderSquare(19)}
-          {this.renderSquare(20)}
-         {this.renderSquare(21)}
-          {this.renderSquare(22)}
-        </div>
-        <div className="board-row">
-
-
-
-          {this.renderSquare(23)}
-          {this.renderSquare(24)}
-          {this.renderSquare(25)}
-        {this.renderSquare(26)}
-             {this.renderSquare(27)}
-        </div>
-          <div className="board-row">
-
-
-
-          {this.renderSquare(28)}
-            {this.renderSquare(29)}
-          {this.renderSquare(30)}
-          {this.renderSquare(31)}
-       {this.renderSquare(32)}
-        </div>
-          <div className="board-row">
-
-
-             {this.renderSquare(33)}
-          {this.renderSquare(34)}
-          {this.renderSquare(35)}
-          {this.renderSquare(36)}
-            {this.renderSquare(37)}
-
-        </div>
       </div>
     );
   }
@@ -146,7 +94,7 @@ class Game extends React.Component {
        squares[9] = "-";
       squares[10] = "*";
       squares[11] = "/";
-      squares[12] = "=";
+      squares[12] = "submit!";
     for (var j = 1; j < 6; j++) {
       var min=1;
       var max=22;
